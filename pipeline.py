9f7f9be512161d73be3a70e296d0c8950d290c57
arkiver <arkiver@protonmail.com>
2021-11-27 14:28:16 +0100

    Version 20211127.01. Update Wget-AT supported version.

--- pipeline.py
+++ pipeline.py
@@ -41,7 +41,7 @@ if StrictVersion(seesaw.__version__) < StrictVersion('0.8.5'):
 
 WGET_AT = find_executable(
     'Wget+AT',
-    ['GNU Wget 1.20.3-at.20210504.01'],
+    ['GNU Wget 1.20.3-at.20211001.01'],
     [
         './wget-at',
         '/home/warrior/data/wget-at'
@@ -57,7 +57,7 @@ if not WGET_AT:
 #
 # Update this each time you make a non-cosmetic change.
 # It will be added to the WARC files and reported to the tracker.
-VERSION = '20211126.02'
+VERSION = '20211127.01'
 USER_AGENT = 'Archive Team'
 TRACKER_ID = 'youtube-dislikes'
 TRACKER_HOST = 'legacy-api.arpa.li'
